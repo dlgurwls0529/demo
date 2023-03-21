402d7c68250bc3b258ee46109d117b29ddac8f38
dlgurwls0529 <dlgurwls0529@gmail.com>
2023-03-21 15:57:32 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/dong/demo/v1/web/dto/WriteAuthsGetResponseDto.java

--- src/main/java/com/dong/demo/v1/web/dto/FoldersGenerateRequestDto.java
+++ src/main/java/com/dong/demo/v1/web/dto/FoldersGenerateRequestDto.java
@@ -3,7 +3,6 @@ package com.dong.demo.v1.web.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter
 public class FoldersGenerateRequestDto {
 
     private final boolean isTitleOpen;
@@ -15,5 +14,17 @@ public class FoldersGenerateRequestDto {
         this.isTitleOpen = isTitleOpen;
         this.symmetricKeyEWF = symmetricKeyEWF;
         this.title = title;
+    }
+
+    public boolean getIsTitleOpen() {
+        return isTitleOpen;
+    }
+
+    public String getSymmetricKeyEWF() {
+        return symmetricKeyEWF;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
