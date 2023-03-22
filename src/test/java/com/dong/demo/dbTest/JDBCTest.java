6c3c3f9837eaa41f3ff873179b566e2ff4c78fc6
dlgurwls0529 <dlgurwls0529@gmail.com>
2023-03-23 08:29:10 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/dong/demo/v1/web/dto/WriteAuthsGetResponseDto.java

--- src/test/java/com/dong/demo/dbTest/JDBCTest.java
+++ src/test/java/com/dong/demo/dbTest/JDBCTest.java
@@ -4,30 +4,36 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 import java.sql.*;
 
 // 안띄우고(띄우기 전에) 주입받은 데이터소스는 프로필 아직 없어서 디폴트인 h2db
 @SpringBootTest
+@PropertySource("classpath:src/main/resources/application-real.properties")
 public class JDBCTest {
 
     @Value("${real.test}")
     private String s;
 
-    @Value("${spring.datasource.url}")
+    // @Value("${spring.datasource.url}")
     private String url;
 
-    @Value("${spring.datasource.username}")
+    // @Value("${spring.datasource.username}")
     private String username;
 
-    @Value("${spring.datasource.password}")
+    // @Value("${spring.datasource.password}")
     private String password;
 
-    @Value("${spring.datasource.driver-class-name}")
+    // @Value("${spring.datasource.driver-class-name}")
     private String driver;
 
     @Test
+    public void propertyTest() {
+        Assertions.assertEquals("test", s);
+    }
+
     public void connectionTest() {
         Assertions.assertEquals("test", s);
 
