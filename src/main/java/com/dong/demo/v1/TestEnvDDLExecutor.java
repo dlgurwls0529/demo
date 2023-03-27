16414e3088babe608984cbf89a3f78e4f33683a5
dlgurwls0529 <dlgurwls0529@gmail.com>
2023-03-28 01:19:50 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/dong/demo/v1/web/dto/WriteAuthsGetResponseDto.java

--- src/main/java/com/dong/demo/v1/TestEnvDDLExecutor.java
+++ src/main/java/com/dong/demo/v1/TestEnvDDLExecutor.java
@@ -23,48 +23,58 @@ public class TestEnvDDLExecutor {
         Connection connection = null;
         connection = DataSourceUtils.getConnection(dataSource);
 
-        String ddl = "" +
-                "CREATE TABLE IF NOT EXISTS Folder (\n" +
+        String ddl1 = "CREATE TABLE IF NOT EXISTS Folder (\n" +
                 "  folderCP CHAR(60) PRIMARY KEY NOT NULL,\n" +
                 "  isTitleOpen BOOL NOT NULL,\n" +
                 "  title TEXT NOT NULL,\n" +
                 "  symmetricKeyEWF TEXT NOT NULL,\n" +
                 "  lastChangedDate TIMESTAMP(6) NOT NULL\n" +
-                ");\n" +
-                "CREATE TABLE IF NOT EXISTS WriteAuthority (\n" +
+                ");";
+
+        String ddl2 = "CREATE TABLE IF NOT EXISTS WriteAuthority (\n" +
                 "  accountCP CHAR(60) NOT NULL,\n" +
                 "  folderCP CHAR(60) NOT NULL,\n" +
                 "  folderPublicKey TEXT NOT NULL,\n" +
                 "  folderPrivateKeyEWA TEXT NOT NULL,\n" +
                 "  PRIMARY KEY (accountCP, folderCP)\n" +
-                ");\n" +
-                "CREATE TABLE IF NOT EXISTS ReadAuthority (\n" +
+                ");";
+
+        String ddl3 = "CREATE TABLE IF NOT EXISTS ReadAuthority (\n" +
                 "  accountCP CHAR(60) NOT NULL,\n" +
                 "  folderCP CHAR(60) NOT NULL,\n" +
                 "  symmetricKeyEWA TEXT NOT NULL,\n" +
                 "  PRIMARY KEY (accountCP, folderCP)\n" +
-                ");\n" +
-                "CREATE TABLE IF NOT EXISTS SubscribeDemand (\n" +
+                ");";
+
+        String ddl4 = "CREATE TABLE IF NOT EXISTS SubscribeDemand (\n" +
                 "  accountCP CHAR(60) NOT NULL,\n" +
                 "  folderCP CHAR(60) NOT NULL,\n" +
                 "  accountPublicKey TEXT NOT NULL,\n" +
                 "  PRIMARY KEY (accountCP, folderCP)\n" +
-                ");\n" +
-                "CREATE TABLE IF NOT EXISTS File (\n" +
+                ");";
+
+        String ddl5 = "CREATE TABLE IF NOT EXISTS File (\n" +
                 "  folderCP CHAR(60) NOT NULL,\n" +
                 "  fileId BINARY(16) NOT NULL,\n" +
                 "  subheadEWS TEXT NOT NULL,\n" +
                 "  lastChangedDate TIMESTAMP(6) NOT NULL,\n" +
                 "  contentsEWS TEXT NOT NULL,\n" +
                 "  PRIMARY KEY (folderCP, fileId)\n" +
-                ");\n" +
+                ");";
+
+        String constraints =
                 "ALTER TABLE WriteAuthority ADD FOREIGN KEY (folderCP) REFERENCES Folder (folderCP);\n" +
                 "ALTER TABLE ReadAuthority ADD FOREIGN KEY (folderCP) REFERENCES Folder (folderCP);\n" +
                 "ALTER TABLE SubscribeDemand ADD FOREIGN KEY (folderCP) REFERENCES Folder (folderCP);\n" +
                 "ALTER TABLE File ADD FOREIGN KEY (folderCP) REFERENCES Folder (folderCP);";
 
         try {
-            connection.prepareStatement(ddl).execute();
+            connection.prepareStatement(ddl1).execute();
+            connection.prepareStatement(ddl2).execute();
+            connection.prepareStatement(ddl3).execute();
+            connection.prepareStatement(ddl4).execute();
+            connection.prepareStatement(ddl5).execute();
+            connection.prepareStatement(constraints).execute();
 
             PreparedStatement preparedStatement = connection.prepareStatement("insert into Folder values('tests', true, 'tests', 'tests', ?);");
             preparedStatement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
