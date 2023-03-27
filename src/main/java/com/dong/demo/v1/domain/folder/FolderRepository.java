b928a6f175c048ba4ff5a8fe0ffaeba24e05b35a
dlgurwls0529 <dlgurwls0529@gmail.com>
2023-03-27 21:57:28 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/dong/demo/v1/web/dto/WriteAuthsGetResponseDto.java

--- src/main/java/com/dong/demo/v1/domain/folder/FolderRepository.java
+++ src/main/java/com/dong/demo/v1/domain/folder/FolderRepository.java
@@ -5,9 +5,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FolderRepository {
-    //todo:명세서 보고 레포지토리 완성. 무결성 제약 조건 주의
     public void save(Folder folder) throws SQLIntegrityConstraintViolationException;
-    public List<Folder> findByFolderCP(String folderCP);
-    public List<Folder> findAll();
+    public List<String[]> findAllFolderCPAndTitle();
     public void updateLastChangedDate(String folderCP, LocalDateTime lastChangedDate);
 }
